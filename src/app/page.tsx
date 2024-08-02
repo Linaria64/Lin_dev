2eea8a156e88ec62f623aa3495ae8b20e28ef38a
Linaria64 <dorianpro77@proton.me>
2024-08-02 19:33:47 +0200

    #7 clean code about page.tsx and setup w and h of container for css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,29 @@ import LinkButton from './component/LinkButton';
 export default function Page() {
   return (
   <div className="bg-background-lin_dev h-screen w-full bg-cover">
-
     <div>
       <Nav/>
+    </div>
 
+    <div className='text-white flex h-2/4 w-full'>
       <div>
         <h1>INSERT PICTURE</h1>
+      </div>  
+
+      <div>
+        <h1> Hello My name is Dorian Soudan Im</h1>
       </div>
 
+      <div>
+        <h2>
+        Passionate about technological innovation, Im a versatile full-stack developer and web designer 
+        with artificial intelligence skills, ready to turn your ideas into robust, aesthetically pleasing solutions.      
+        </h2>
+      </div>
     </div>
 
-    <div>
-      <h1> Hello My name is Dorian Soudan Im</h1>
-    </div>
-
-    <div>
-      <h2>
-      Passionate about technological innovation, Im a versatile full-stack developer and web designer 
-      with artificial intelligence skills, ready to turn your ideas into robust, aesthetically pleasing solutions.      
-      </h2>
-    </div>
-
+   
+  
     <NeonButton/>
 
     <NeonButton/>
