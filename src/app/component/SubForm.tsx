cdd16a80bcc29846f4a2a44634fe7b2c596d2752
Linaria64 <dorianpro77@proton.me>
2024-08-04 13:25:58 +0200

    #17 Contact form design

--- src/app/component/SubForm.tsx
+++ src/app/component/SubForm.tsx
@@ -2,10 +2,12 @@
 
 export default function ContactForm () {
     return (
-        <div>
-            <div><p>Strudergnach</p></div>
-            <div><p>Leo</p></div>
-            <div><p>Imanemail@mail.xx</p></div>
+        <div  className="bg-gradient-to-r from-[#181818] via-[#44424A] to-[#BFBFBF] w-2/4 h-full rounded-lg mx-auto text-center">
+            <div className="border-2 border-blue-800 w-3/4 mx-auto rounded-lg "><p className="border-2 border-blue-500 w-2/4 mx-auto p-2 my-2 rounded-lg">Strudergnach</p></div>
+            <br />
+            <div className="border-2 border-blue-800 w-3/4 mx-auto rounded-lg"><p className="border-2 border-blue-500 w-2/4 mx-auto p-2 my-2 rounded-lg">Leo</p></div>
+            <br />
+            <div className="border-2 border-blue-800 w-3/4 mx-auto rounded-lg"><p className="border-2 border-blue-500 w-2/4 mx-auto p-2 my-2 rounded-lg">Imanemail@mail.xx</p></div>
         </div>
     )
-}
+} 
