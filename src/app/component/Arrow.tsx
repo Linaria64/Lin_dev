024121e78b685fc75b9b97d0cf868b1f28ef4c7e
Linaria64 <dorianpro77@proton.me>
2024-08-04 12:17:03 +0200

    #16 Settup click for arrow

--- src/app/component/Arrow.tsx
+++ src/app/component/Arrow.tsx
@@ -1,11 +1,14 @@
 'use client'
+import Link from 'next/link'
 import { FaArrowUp } from "react-icons/fa";
 
 export default function arrow () {
     return (
         <div className="">
-            
-            <FaArrowUp  className="w-20 h-20 fill-blue-950 border-2 border-white rounded-3xl"  />
+            <Link href='/'>
+                        <FaArrowUp  className="w-20 h-20 fill-blue-950 border-2 border-white rounded-3xl"  />
+
+            </Link>
         </div>
     )
 }
